26087771f370ab43e8b6e89dd10ee6707f61654b
Mohammad Alaa Aldein <mohammad.aladin996@gmail.com>
2023-09-29 23:41:04 +0300

    fix wrong enter visit reminder every time and firebase configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,41 +3,41 @@ import { Router } from '@angular/router';
 import { LightService } from './services/light.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  itIsFirstTime = false;
+	itIsFirstTime = false;
 
-  readonly oneDayUnix: number = 84600;
+	readonly oneDayUnix: number = 84600;
 
-  constructor(
-    private router: Router,
-    private light: LightService
-  ) { }
+	constructor(
+		private router: Router,
+		private light: LightService
+	) { }
 
-  ngOnInit() {
-    this.checkLastVisitTime();
+	ngOnInit() {
+		this.checkLastVisitTime();
 
-    ["memorizing", "interpretation", "lastReadingTime", "lastListeningTime"].forEach((key) => {
-      if (!localStorage.getItem(key)) {
-        localStorage.setItem(key, JSON.stringify(Array.from({ length: 144 }, () => 0)));
-      }
-    });
-    this.light.init();
-  }
+		["memorizing", "interpretation", "lastReadingTime", "lastListeningTime"].forEach((key) => {
+			if (!localStorage.getItem(key)) {
+				localStorage.setItem(key, JSON.stringify(Array.from({ length: 144 }, () => 0)));
+			}
+		});
 
-  checkLastVisitTime() {
-    const lastVisitTime = +(localStorage.getItem('last-visit-reminder') || '');
-    if (!lastVisitTime)
-      return this.router.navigate(['visit_reminder']);
+		this.light.init();
+	}
 
-    if ((Date.now() - lastVisitTime / 1000) >= this.oneDayUnix) {
-      return this.router.navigate(['visit_reminder']);
-    }
+	checkLastVisitTime() {
+		const lastVisitTime = +(localStorage.getItem('last-visit-reminder') || '');
+		if (!lastVisitTime)
+			return this.router.navigate(['visit_reminder']);
 
-    return;
-  }
+		if ((Date.now() - lastVisitTime) / 1000 >= this.oneDayUnix)
+			return this.router.navigate(['visit_reminder']);
+
+		return;
+	}
 
 }
