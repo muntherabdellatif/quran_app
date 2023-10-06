c7ee8cffb5f7da34b5cd258537f073480333864d
Mohammad Alaa Aldein <mohammad.aladin996@gmail.com>
2023-10-06 20:37:55 +0300

    update mobile app configs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.quran.app',
-  appName: 'quran-light',
+  appName: 'Quran',
   webDir: 'dist/quran_light',
   server: {
     androidScheme: 'https'
